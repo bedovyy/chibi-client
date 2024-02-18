93147f7e21472b6166bc131efd999a300793c33a
bedovyy <bedovyy@gmail.com>
2024-02-18 23:41:17 +0900

    remove hashes on dist files

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   }
 })
